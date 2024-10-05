b12429b89330d908702ddea0ddbc84cae733cde0
league <league@adminisorsimac3.attlocal.net>
2024-10-05 13:00:36 -0700

    sfs

---
+++ src/BlockJump.java
@@ -0,0 +1,13 @@
+
+public class BlockJump {
+
+	BlockJump(){
+
+	}
+	public static void main(String[] args) {
+		size(1000,1000);
+	}
+	void draw() {
+
+	}
+}
---
+++ src/Course.java
@@ -0,0 +1,21 @@
+
+public class Course {
+	Course(){
+
+	}
+	void Course1() {
+
+	}
+	void Course2() {
+
+	}
+	void Course3() {
+
+	}
+	void Course4() {
+
+	}
+	void Course5() {
+
+	}
+}
