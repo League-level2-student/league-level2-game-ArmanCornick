7dc89b0f2cb27421c76f39ba9e93e4a6e339a8a4
league <league@imac-22.attlocal.net>
2024-10-17 18:34:08 -0700

    fwefw

--- src/BlockJump.java
+++ src/BlockJump.java
@@ -1,13 +1,66 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class BlockJump {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class BlockJump extends JPanel implements KeyListener, ActionListener {
+	JFrame frame;
+	Cube wave;
+	Timer time;
 
 	BlockJump(){
-
+		time = new Timer(1000/60, this);
+		frame = new JFrame();
+		wave = new Cube();
+		frame.add(this);
+		this.setPreferredSize(new Dimension(1000,1000));
+		frame.setVisible(true);
+		frame.pack();
+		time.start();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addKeyListener(this);
 	}
 	public static void main(String[] args) {
-		size(1000,1000);
+		new BlockJump();
 	}
-	void draw() {
 
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		wave.updateMovement();
+		repaint();
+	}
+
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_UP) {
+			wave.goingUp = true;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		wave.goingUp = false;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void paintComponent(Graphics g) {
+		// TODO Auto-generated method stub
+		g.setColor(Color.cyan.darker());
+		g.fillRect(0,0,1000,1000);
+		wave.draw(g);
+	
 	}
 }
--- src/Cube.java
+++ src/Cube.java
@@ -1,6 +1,27 @@
-
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 public class Cube {
+	int y;
+	Boolean goingUp;
+	
 	Cube(){
+		y = 460;
+		goingUp = false;
+	}
+	void updateMovement() {
+		if(goingUp==false) {
+			y+=4;
+		} else {
+			y-=4;
+		}
+	}
 
+	public void draw(Graphics g) {
+		g.setColor(Color.black);
+		g.fillRect(280,y,80,40);
 	}
 }
